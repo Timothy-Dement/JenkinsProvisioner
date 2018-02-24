01e07735b447c6eebb5f1a248733d6054a6479c5
Timothy-Dement <tmdement@ncsu.edu>
2018-02-24 15:03:34 -0500

    Update config

--- templates/configure-users.groovy
+++ templates/configure-users.groovy
@@ -1,28 +1,42 @@
 #!groovy
-import hudson.security.*
+
+import jenkins.install.*
 import jenkins.model.*
 
+import hudson.security.*
+import hudson.util.*
+
 def instance = Jenkins.getInstance()
+
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+
 def users = hudsonRealm.getAllUsers()
+
 users_s = users.collect { it.toString() }
 
-// Create the admin user account if it doesn't already exist.
-if ("{{ lookup('env', 'JENKINS_USERNAME') }}" in users_s) {
-    println "{{ lookup('env', 'JENKINS_USERNAME') }} user already exists - updating password"
-
+if ("{{ lookup('env', 'JENKINS_USERNAME') }}" in users_s)
+{
     def user = hudson.model.User.get("{{ lookup('env', 'JENKINS_USERNAME') }}")
-    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword("{{ lookup('env', 'JENKINS_PASSWORD') }}")
-    user.addProperty(password)
-    user.save()
-}
-else {
-    println "--> creating local {{ lookup('env', 'JENKINS_USERNAME') }} user"
 
+    def password = hudson.security.HudsonPrivateSecurityRealm.Details.fromPlainPassword("{{ lookup('env', 'JENKINS_PASSWORD') }}")
+
+    user.addProperty(password)
+
+    user.save()
+
+    instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
+}
+else
+{
     hudsonRealm.createAccount("{{ lookup('env', 'JENKINS_USERNAME') }}", "{{ lookup('env', 'JENKINS_PASSWORD') }}")
+
     instance.setSecurityRealm(hudsonRealm)
 
     def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
+ 
     instance.setAuthorizationStrategy(strategy)
+ 
     instance.save()
+
+    instance.setInstallState(InstallState.INITIAL_SETUP_COMPLETED)
 }
